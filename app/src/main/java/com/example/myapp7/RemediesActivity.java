f8c448d900aaeff33b0d83ce10b95ae1fab3d5ab
Krishna Dammavalam <kdammavalam@reputation.com>
2024-10-10 10:57:39 +0530

    config

--- app/src/main/java/com/example/myapp7/RemediesActivity.java
+++ app/src/main/java/com/example/myapp7/RemediesActivity.java
@@ -1,9 +1,19 @@
 package com.example.myapp7;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
 
 public class RemediesActivity extends BaseActivity {
 
@@ -13,6 +23,11 @@ public class RemediesActivity extends BaseActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_remedies);
+
+        // Load the selected locale from SharedPreferences
+        SharedPreferences sharedPreferences = getSharedPreferences("LocalePrefs", MODE_PRIVATE);
+        String selectedLocale = sharedPreferences.getString("selected_locale", "en"); // Default to English
+
 
         // Find the TextViews in the layout
         attributeTextView = findViewById(R.id.attribute_text);
@@ -24,6 +39,50 @@ public class RemediesActivity extends BaseActivity {
 
         // Set the text for the TextViews
         //attributeTextView.setText("Below of are the Remedies for the sideeffect: " + attribute);
-        valueTextView.setText("Rememdy: " + value);
+        ArrayList<String> seList = loadJsonData(attribute, selectedLocale);
+        seList.forEach(e-> valueTextView.setText("Remedy: " + e));
+    }
+
+    private ArrayList<String> loadJsonData(String symptomName, String locale) {
+        System.out.print("The symtom received was: " + symptomName);
+        // Implement your JSON loading logic here
+        ArrayList<String> data = new ArrayList<>();
+        try {
+
+            String filename = "";
+            if("en".equalsIgnoreCase(locale)) {
+                filename = "drugmodel.json";
+            } else {
+                filename = "drugmodel-"+locale+".json";
+            }
+            System.out.println("Remedy filename loaded for the data is::" + filename);
+            BufferedReader reader = new BufferedReader(new InputStreamReader(getAssets().open(filename)));
+            JsonObject rootjsonObject = JsonParser.parseReader(reader)
+                    .getAsJsonObject();
+            JsonArray drugarray = rootjsonObject.getAsJsonArray("drugs");
+            for(int i=0;i<drugarray.size();i++){
+                JsonObject drugObject = drugarray.get(i).getAsJsonObject();
+                JsonArray searray = drugObject.getAsJsonArray("sideeffects");
+                for(int j=0;j<searray.size();j++) {
+                    JsonObject  seObject = searray.get(j).getAsJsonObject();
+                    String seResult = seObject.get("sname").getAsString();
+                    System.out.println("Remedies --->>  Side Effect Name --" + seResult);
+                    if (seResult.equals(symptomName)) {
+                        JsonArray remarray = seObject.getAsJsonArray("Remedies");
+                        for(int k=0;k<remarray.size();k++) {
+                            JsonObject remObject = remarray.get(k).getAsJsonObject();
+                            String remResult = remObject.get("rdescription").getAsString();
+                            System.out.println("Remedy  --" + remResult);
+                            data.add(remResult);
+                        }
+                    }
+                }
+            }
+            reader.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        //return data;
+        return data;
     }
 }
